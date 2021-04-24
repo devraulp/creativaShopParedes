2a92e04502f99edab269a65b648f6f084cea02d3
Raul Paredes <raul_paredes_s@hotmail.com>
2021-04-24 17:50:27 -0300

    Caja de Articulo y Selector de unidades

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
-
 import {NavBar} from "./components/NavBar/NavBar";
-
 import {ItemListContainer} from "./components/ItemListContainer/ItemListContainer";
-
+import {ItemCount} from "./components/ItemCount/ItemCount"
 
 
 function App() {
@@ -11,6 +9,7 @@ function App() {
     <div>
       <NavBar/>
       <ItemListContainer greeting="Lista de Ariculos"/>
+      <ItemCount stock={5} initial={1} onAdd={1} />
     </div>    
   )
 }
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import "./ItemCount.css"
+
+
+export const ItemCount = ({stock, initial, onAdd}) => {        
+
+    const [count, setCount] = useState(1);
+
+    function restar () {
+        if(count > initial){
+        setCount(count - onAdd);
+        }
+    }
+
+    function sumar () {
+        if(count < stock){
+        setCount(count + onAdd);
+        }else {
+            alert("No hay mas Stock");
+        }
+    }
+    
+    return ( 
+        <div className="itemCount" >
+            <p>Articulo</p>
+            <div>
+                <button onClick={restar}> - </button>
+                <p type="number"> {count} </p>
+                <button onClick={sumar}> + </button>                
+            </div>
+            <p> Stock Disponible : {stock} unidades </p>
+        </div>
+        
+    )
+}
